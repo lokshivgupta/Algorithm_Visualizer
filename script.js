987c8701725f11f87999f90d9534f4c41150fd5f
Lokshiv Gupta <guptalokshiv@gmail.com>
2026-02-02 02:50:17 -0800

    Phase 4 Completed

--- script.js
+++ script.js
@@ -2,45 +2,51 @@ let container = document.querySelector(".container");
 
 
 
-for(let i = 0; i < 50; i++){
-    let newBar = document.createElement("div");
-    newBar.classList.add("bar");
-    let randomHeight = Math.floor(Math.random() * 100)
-    newBar.style.height =  randomHeight + "px"
-    container.append(newBar);
-}
-
-let waitFor = (milisec)=>{ 
-    return new Promise(resolve => { 
-        setTimeout(() => { 
-            resolve(); 
-        }, milisec); 
-    }); 
-}
-
-let bars = document.querySelectorAll(".bar");
-let  swap =  async ()=>{
-    for(let i = 0; i < bars.length -1; i++){
-    //Highlight Logic
-    bars[i].style.backgroundColor = "blue";
-    bars[i+1].style.backgroundColor = "blue";
-    await waitFor(2000);
-
-    //Swap Logic
-    
-        let val1 = parseInt(bars[i]?.style.height);
-        let val2 = parseInt(bars[i+1]?.style.height);
-        let temp = val1;
-
-        if(temp > val2){
-            bars[i].style.height = val2 + "px";
-            bars[i+1].style.height = temp + "px";
-        }
-        bars[i].style.backgroundColor = "red";
-        bars[i+1].style.backgroundColor = "red";
+document.getElementById("btn").addEventListener("click", ()=>{
+    for (let i = 0; i < 50; i++) {
+        let newBar = document.createElement("div");
+        newBar.classList.add("bar");
+        let randomHeight = Math.floor(Math.random() * 100)
+        newBar.style.height = randomHeight + "px"
+        container.append(newBar);
     }
-    
-};
+} );
 
-swap();
+
+let waitFor = (milisec) => {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve();
+        }, milisec);
+    });
+}
+
+
+document.getElementById("btn2").addEventListener("click" , async ()=>{
+    let bars = document.querySelectorAll(".bar");
+    for (let i = 0; i < bars.length - 1; i++) {
+        for (let j = 0; j < bars.length - 1 - i; j++) {
+            //Highlight Logic
+            bars[j].style.backgroundColor = "blue";
+            bars[j + 1].style.backgroundColor = "blue";
+            await waitFor(200);
+
+            //Swap Logic
+
+            let val1 = parseInt(bars[j]?.style.height);
+            let val2 = parseInt(bars[j + 1]?.style.height);
+            let temp = val1;
+
+            if (temp > val2) {
+                bars[j].style.height = val2 + "px";
+                bars[j + 1].style.height = temp + "px";
+            }
+            bars[j].style.backgroundColor = "red";
+            bars[j+ 1].style.backgroundColor = "red";
+        }
+        bars[bars.length - 1 - i].style.backgroundColor = "green";
+
+    }
+});
+
 
